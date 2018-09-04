564d5e104a66a7f4fc7df4814f979cec1f10b369
Mohamed <Ethical.Hacker720@gmail.com>
2018-09-04 10:08:54 -0400

    Update pathfinder.py

--- builder/lib/pathfinder.py
+++ builder/lib/pathfinder.py
@@ -9,7 +9,6 @@ from getpass import getuser
 class Finder(object):
 
  root_dir = os.path.abspath(os.path.sep) + os.path.sep + 'Users' + os.path.sep + getuser() + os.path.sep + 'AppData'
- paths = []
 
  def is_bad(root, dirs, files):
   return not all([len(dirs), len(files), len(os.path.normpath(root).split(os.sep)) >= randint(5, 10)])   
@@ -21,8 +20,13 @@ class Finder(object):
 
  @classmethod
  def find(cls):
+  paths = []
   for root, dirs, files in os.walk(cls.root_dir, topdown=True):
    if cls.is_bad(root, dirs, files):continue
-   path = root + os.path.sep + cls.choice(dirs)
-   cls.paths.append(path)
-  return cls.choice(cls.paths) + os.path.sep
+   _dir = cls.choice(dirs)
+   if '.' in _dir:
+    continue
+   else:
+    path = root + os.path.sep + _dir
+    paths.append(path)
+  return cls.choice(paths) + os.path.sep
